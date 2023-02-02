da024c7b4e70baa0a7aa68c21775dd1d4abea1e5
Andhikanurf <97925983+Andhikanurf@users.noreply.github.com>
2023-02-02 10:32:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ def prediction(age, sex, bmi, children, smoker):
 
 def main():
     st.title("Aplikasi Prediksi Asuransi dengan Algoritma Linier Regression")
-    st.markdown('Nama : Saskia Bintang Maharani | NIM : 2019230047')
+    st.markdown('Andhika Nur Firdaus (2019230061)')
     st.write('\n')
-    st.markdown('Silakan isi form berikut terlebih dahulu :')
+    st.markdown('Silakan Lengkapi Form Berikut:')
     
     st.write('\n')
     age = st.number_input("Age", 0)
     sex = st.number_input("Sex (Laki-Laki = 0, Perempuan = 1)", 0)
     bmi = st.number_input("BMI", 0)
     children = st.number_input("Children", 0)
-    smoker = st.number_input("Smoker (Smoker = 1, Non Smoker = 0)", 0)
+    smoker = st.number_input("Smoker (Ya = 1, Tidak = 0)", 0)
     result =""
     
     if st.button("Predict"):
